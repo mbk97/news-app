c0cf580c3c3907a41f1406e07e7b670b9a91c4a0
mubarak.muhammed <mubarak.muhammed@wemabank.com>
2024-09-20 10:25:37 +0100

    update

--- controller/news.ts
+++ controller/news.ts
@@ -38,4 +38,83 @@ const createNews = async (req: Request, res: Response) => {
   }
 };
 
-export { createNews };
+const getAllNews = async (req: Request, res: Response) => {
+  const news = await News.find();
+  res.status(200).json({
+    data: news,
+    message: "Successful",
+  });
+};
+
+const getNewById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const news = await News.findById(id);
+  if (news) {
+    res.status(200).json({
+      data: news,
+      message: "Successful",
+    });
+  } else {
+    res.status(400).json({
+      message: "News not found",
+    });
+    return;
+  }
+};
+
+const updateNews = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const error = validate(createNewsSchema, req.body);
+  if (error) {
+    res.status(400).json({
+      message: error,
+    });
+    return;
+  }
+  try {
+    const newsToBeUpdated = await News.findById(id);
+    if (!newsToBeUpdated) {
+      res.status(400).json({
+        message: "News does not exist",
+      });
+      return;
+    }
+
+    const data = await News.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    if (data) {
+      res.status(200).json({
+        message: "News updated",
+        data,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+const deleteNews = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const news = await News.findById(id);
+    if (!news) {
+      res.status(400).json({
+        message: "News does not exist",
+      });
+      return;
+    } else {
+      await news.remove();
+      res.status(200).json({ message: "News deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export { createNews, getAllNews, updateNews, getNewById, deleteNews };
